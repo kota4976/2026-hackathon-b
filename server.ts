be1ff7a517a5f66083bbb1669fe10f59c8547eee
ha-suzuki <ha-suzuki@jig.jp>
2026-03-10 13:52:39 +0900

    現在のカテゴリーごとのスレッドをMapで管理するように変更

--- server.ts
+++ server.ts
@@ -11,20 +11,44 @@ let categories: Category[] = [
   { categoryId: 3, name: "仕事" },
 ];
 
-const threads = [
-  {
-    threadId: "ああああ",
-    name: "いいいい",
-    title: "うううう",
-    count: 2,
-  },
-  {
-    threadId: "eeee",
-    name: "oooo",
-    title: "12支",
-    count: 5,
-  },
-];
+type Threads = {
+  threads: Thread[];
+};
+
+type Thread = {
+  threadId: number;
+  name: string;
+  title: string;
+};
+
+// カテゴリーごとのスレッドを管理するMap
+const threads = new Map<number, Threads>();
+threads.set(1, {
+  threads: [
+    {
+      threadId: 1,
+      name: "テスト1",
+      title: "テスト1のタイトル",
+    },
+    {
+      threadId: 2,
+      name: "テスト2",
+      title: "テスト2のタイトル",
+    },
+  ],
+});
+threads.set(2, {
+  threads: [
+    { threadId: 3, name: "研究1", title: "研究1のタイトル" },
+    { threadId: 4, name: "研究2", title: "研究2のタイトル" },
+  ],
+});
+threads.set(3, {
+  threads: [
+    { threadId: 5, name: "仕事1", title: "仕事1のタイトル" },
+    { threadId: 6, name: "仕事2", title: "仕事2のタイトル" },
+  ],
+});
 
 Deno.serve((req) => {
   const url = new URL(req.url);
