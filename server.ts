be2b2c4987af375c0787debf82288aac0207e7c9
sbsb <youshensechuan@gmail.com>
2026-03-10 10:17:51 +0900

    getcategoryを返すようにした。

---
+++ server.ts
@@ -0,0 +1,19 @@
+type Category = {
+    categoryId: number;
+    name: string;
+}
+
+let categories: Category[] = [
+    { categoryId: 1, name: "テスト" },
+    { categoryId: 2, name: "研究" },
+    { categoryId: 3, name: "仕事" },
+]
+Deno.serve((req) => {
+  if (req.method === "GET" && new URL(req.url).pathname === "/category") {
+    return new Response(JSON.stringify(categories), {
+    headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  return new Response("Not Found", { status: 404 });
+});
